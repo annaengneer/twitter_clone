4e28d4d42626c5a363cce143f06fe5dda4563557
anna <kokoan438@gmail.com>
2025-09-05 17:07:40 +0900

    fix adapter.py ver3

--- twitter_app/adapter.py
+++ twitter_app/adapter.py
@@ -5,7 +5,5 @@ class CustomAccountAdapter(DefaultAccountAdapter):
     print("generate_unique_username 呼び出し OK")
     def generate_unique_username(self, txts, regex=None):
         base_username = '@githubuser'
-        return generate_unique_username(
-            [base_username],
-            regex=r'^@[\w]+$',
-            )
+        candidates = [f'{base_username}' for i in range(1, 1000)]
+        return generate_unique_username(candidates,regex=r'^@[\w]+$')
