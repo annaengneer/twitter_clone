01eda071236b8512d8590ac8d85e1bfff146da48
anna <kokoan438@gmail.com>
2025-09-05 17:20:24 +0900

    fix adapter.py ver4

--- twitter_app/adapter.py
+++ twitter_app/adapter.py
@@ -2,8 +2,6 @@ from allauth.account.adapter import DefaultAccountAdapter
 from allauth.utils import generate_unique_username
 
 class CustomAccountAdapter(DefaultAccountAdapter):
-    print("generate_unique_username 呼び出し OK")
     def generate_unique_username(self, txts, regex=None):
-        base_username = '@githubuser'
-        candidates = [f'{base_username}' for i in range(1, 1000)]
+        candidates = [f'@githubuser{i}' for i in range(1, 1000)]
         return generate_unique_username(candidates,regex=r'^@[\w]+$')
